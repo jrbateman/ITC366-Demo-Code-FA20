cd7c71c3fe210acfa349fac29c38ef9d25377b4f
James Bateman <jrbatemanjr@gmail.com>
2020-06-13 16:51:50 -0500

    06132020.3

---
+++ Chapter6/IDrivable.cs
@@ -0,0 +1,16 @@
+﻿namespace Chapter6
+{
+    internal interface IDrivable
+    {
+        // Interfaces can have properties
+        int Wheels { get; set; }
+        double Speed { get; set; }
+
+        // Classes that inherit an interface
+        // must fulfill the contract and 
+        // implement every abstract method
+        void Move();
+        void Stop();
+
+    }
+}
---
+++ Chapter6/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter6
+{
+    class Vehicle: IDrivable
+    {
+
+        // Vehicle properties
+        public string Brand { get; set; }
+
+        // Properties and methods from
+        // the interface
+        public double Speed { get; set; }
+        public int Wheels { get; set; }
+
+        //Constructor
+        public Vehicle(string brand = "No Brand",
+                int wheels = 0,
+                double speed = 0)
+        {
+            Brand = brand;
+            Wheels = wheels;
+            Speed = speed;
+        }
+
+
+        public void Move()
+        {
+            Console.WriteLine($"The {Brand} Moves Forward at {Speed} MPH");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"The {Brand} Stops");
+            Speed = 0;
+        }
+    }
+}
+}
