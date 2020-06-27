62c45b06642065b7fb40578a446c1e0c71e3e240
James Bateman <jrbatemanjr@gmail.com>
2020-06-27 17:56:49 -0500

    06272020.3 Added Chapter 7&8 Methods

--- Chapter7and8/Program.cs
+++ Chapter7and8/Program.cs
@@ -2,9 +2,9 @@
 
 namespace Chapter7and8
 
-    // Methods
+// Methods
 
-{
+    {
     class Program
         {
         static void Main(string[] args)
@@ -48,12 +48,12 @@ namespace Chapter7and8
 
             // Returning Values from Methods
 
-            double myHours = 37.5; 
-            double myRate = 12.75; 
+            double myHours = 37.5;
+            double myRate = 12.75;
             double grossPay;
-            
+
             Console.WriteLine("I worked {0} hours at {1} per hour", myHours, myRate);
-            
+
             grossPay = CalcPay(myHours, myRate);
 
             Console.WriteLine("My gross pay is {0}", grossPay.ToString("C"));
@@ -66,15 +66,85 @@ namespace Chapter7and8
             Console.WriteLine("TestInput(5) = {0}", TestInput(5));
             Console.WriteLine("TestInput(11) = {0}", TestInput(11));
             Console.WriteLine();
-            }
 
+            // Passing Array Values to a Method
+            // Passing by reference vs value (values change in the method)
+
+            int[] someNums = { 10, 12, 22, 35 };
+            int x;
+
+            Console.Write("\nAt beginning of Main() method...   ");
+            for (x = 0; x < someNums.Length; ++x)
+                Console.Write("{0, 6}", someNums[x]);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            MethodGetsArray(someNums);
+            Console.WriteLine();
+
+            Console.Write("At end of Main() method..........");
+            for (x = 0; x < someNums.Length; ++x)
+                Console.Write("{0, 6}", someNums[x]);
+
+            Console.WriteLine();
+
+
+            // ************************************ {Part 2 Second Set of lectures)
+
+
+            // Passing Parameters by Reference
+
+            int xx = 4;
+            Console.WriteLine();
+            Console.WriteLine("In Main x is {0}", xx);
+            DisplayReferenceParameter(ref xx);
+            Console.WriteLine("In Main x is {0}", xx);
+            Console.WriteLine();
+
+            // Returning a Reference from a Method
+
+            int xxx = 4;
+            Console.WriteLine();
+            Console.WriteLine("In Main x is {0}", xxx);
+            DisplayValueParameter(xxx);
+            Console.WriteLine("In Main x is {0}", xxx);
+
+            // Overloading Methods
+
+
+            //Overloading involves the ability to write multiple versions of a method using the same method name. 
+            //When you make a purchase at a store, you might use one of a variety of “pay” methods they accept, for example, cash, a credit card, or a check.The “pay” method works differently depending on the type of currency sent to it, but all of the transactions are called “pay.” 
+            //When you overload a C# method, you write multiple method implementations with the same name but different parameter lists. 
+            //A method’s name and parameter list constitute the method’s signature.
+
+            Console.WriteLine();
+            DisplayWithBorder("Ed");
+            DisplayWithBorder(3);
+            DisplayWithBorder(456);
+            DisplayWithBorder(897654);
+            DisplayWithBorder("Veronica");
+          
+
+            }
+        private static void DisplayValueParameter(int number)
+            {
+
+            number = 888;
+            Console.WriteLine("In method, number is {0}", number);
+            }
+        private static void DisplayReferenceParameter(ref int number)
+            {
+
+            number = 888;
+            Console.WriteLine("In method, number is {0}", number);
+
+            }
         private static double CalcPay(double myHours, double myRate)
             {
-                double gross; 
-                gross = myHours * myRate; 
-                return gross;
+            double gross;
+            gross = myHours * myRate;
+            return gross;
             }
-
         private static void ShowWelcomeMessage()
             {
 
@@ -111,6 +181,67 @@ namespace Chapter7and8
                 return false;
             return true;
             }
+        private static void MethodGetsArray(int[] vals)
+            {
 
+            int x;
+
+            Console.Write("In MethodGetsArray() ");
+
+            for (x = 0; x < vals.Length; ++x)
+                Console.Write(" {0}", vals[x]);
+            Console.WriteLine();
+
+            for (x = 0; x < vals.Length; ++x)
+                vals[x] = 888;
+            Console.Write("After change");
+
+            for (x = 0; x < vals.Length; ++x)
+                Console.Write(" {0}", vals[x]);
+            Console.WriteLine();
+
+
+            }
+        private static void DisplayWithBorder(string word)
+            {
+
+            const int EXTRA_STARS = 4;
+            const string SYMBOL = "*";
+            int size = word.Length + EXTRA_STARS;
+            int x; for (x = 0; x < size; ++x)
+                Console.Write(SYMBOL);
+
+            Console.WriteLine();
+            Console.WriteLine(SYMBOL + " " + word + " " + SYMBOL);
+            for (x = 0; x < size; ++x)
+                Console.Write(SYMBOL);
+
+            Console.WriteLine("\n\n");
+            }
+        private static void DisplayWithBorder(int number)
+            {
+
+            const int EXTRA_STARS = 4;
+            const string SYMBOL = "*";
+            int size = EXTRA_STARS + 1;
+            int leftOver = number;
+            int x;
+
+            while (leftOver >= 10)
+                {
+                leftOver = leftOver / 10; ++size;
+                }
+            for (x = 0; x < size; ++x)
+                Console.Write(SYMBOL);
+
+            Console.WriteLine();
+            Console.WriteLine(SYMBOL + " " + number + " " + SYMBOL);
+
+            for (x = 0; x < size; ++x)
+                Console.Write(SYMBOL);
+
+            Console.WriteLine("\n\n");
+
+            }
         }
-}
+    }
